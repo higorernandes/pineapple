5750f0df65faad9f0c999904aeba7daaf2a39145
Higor Ernandes <higorernandes@gmail.com>
2017-07-12 03:15:41 -0300

    Addition of login screen and Facebook login process.

---
+++ app/src/main/java/pineapplesoftware/pineappleapp/account/view/LoginActivity.kt
@@ -0,0 +1,117 @@
+package pineapplesoftware.pineappleapp.account.view
+
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.view.View
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+
+import kotlinx.android.synthetic.main.activity_login.*
+import pineapplesoftware.pineappleapp.R
+import pineapplesoftware.pineappleapp.main.view.MainActivity
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
+class LoginActivity : AppCompatActivity(), FacebookCallback<LoginResult>, View.OnClickListener {
+
+    //region Attributes
+
+    private var TAG : String = "LoginActivity"
+
+    private var mCallbackManager : CallbackManager? = null
+
+    //endregion
+
+    //region Overridden Methods
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        setUpFacebookLogin()
+
+        prepareViews()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        mCallbackManager?.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onSuccess(result: LoginResult?) {
+        Log.d(TAG, "Login successful - UserID: ${result?.accessToken?.userId}}, Auth Token: ${result?.accessToken?.token}.")
+        val intent : Intent = MainActivity.getActivityIntent(this, result?.accessToken?.userId)
+        startActivity(intent)
+    }
+
+    override fun onCancel() {
+        Log.d(TAG, "Login attempt has been canceled.")
+    }
+
+    override fun onError(error: FacebookException?) {
+        Log.e(TAG, "Could not log onto Facebook: ${error?.message}.")
+    }
+
+    override fun onClick(v: View?) { }
+
+    //endregion
+
+    //region Private Methods
+
+    private fun setUpFacebookLogin() {
+        FacebookSdk.sdkInitialize(applicationContext)
+        mCallbackManager = CallbackManager.Factory.create()
+        LoginManager.getInstance().registerCallback(mCallbackManager, this)
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "user_friends", "email"))
+    }
+
+    private fun prepareViews() {
+        facebookLoginButton.setOnClickListener(this)
+        emailSignInButton.setOnClickListener { attemptLogin() }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        //TODO: Replace this with your own logic
+        return email.contains("@")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        //TODO: Replace this with your own logic
+        return password.length > 4
+    }
+
+    /**
+     * This method gets the App's HashKey for Facebook authentication. Used for test purposes only.
+     */
+    private fun getFacebookAppHashKey() {
+        try {
+            var info : PackageInfo = packageManager.getPackageInfo("pineapplesoftware.pineappleapp", PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val messageDigest : MessageDigest = MessageDigest.getInstance("SHA")
+                messageDigest.update(signature.toByteArray())
+                Log.d(TAG, "KeyHash: ${Base64.encodeToString(messageDigest.digest(), Base64.DEFAULT)}")
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            Log.e(TAG,"The package name could not be found.")
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e(TAG, "Could not find the given algorithm.")
+        }
+    }
+
+    private fun attemptLogin() {
+
+    }
+
+    //endregion
+}
--- app/src/main/java/pineapplesoftware/pineappleapp/main/view/MainActivity.kt
+++ app/src/main/java/pineapplesoftware/pineappleapp/main/view/MainActivity.kt
@@ -1,5 +1,9 @@
 package pineapplesoftware.pineappleapp.main.view
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
@@ -7,11 +11,13 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
+import com.facebook.login.widget.ProfilePictureView
 
 import pineapplesoftware.pineappleapp.R
 import pineapplesoftware.pineappleapp.main.adapter.TransactionsListArrayAdapter
 import pineapplesoftware.pineappleapp.main.model.AccountDto
 import pineapplesoftware.pineappleapp.main.model.TransactionItemDto
+import java.net.URL
 import java.util.*
 
 class MainActivity : AppCompatActivity() , View.OnClickListener
@@ -19,8 +25,10 @@ class MainActivity : AppCompatActivity() , View.OnClickListener
     //region Attributes
 
     private var mMainExpensesRecyclerView : RecyclerView? = null
+    private var mUserProfilePictureView: ProfilePictureView? = null
 
     private var mObjects : ArrayList<TransactionItemDto> = ArrayList<TransactionItemDto>()
+    private var mUserId : String? = null
 
     //endregion
 
@@ -29,6 +37,8 @@ class MainActivity : AppCompatActivity() , View.OnClickListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mUserId = intent.getStringExtra(USER_ID) ?: throw IllegalStateException("Field $USER_ID missing in Intent.")
 
         loadData()
         getViews()
@@ -55,11 +65,35 @@ class MainActivity : AppCompatActivity() , View.OnClickListener
 
     private fun getViews() {
         mMainExpensesRecyclerView = findViewById(R.id.mainExpensesRecyclerView) as RecyclerView?
+        mUserProfilePictureView = findViewById(R.id.profile_picture) as ProfilePictureView?
+
 
         mMainExpensesRecyclerView?.layoutManager = LinearLayoutManager(this)
         mMainExpensesRecyclerView?.itemAnimator = DefaultItemAnimator()
         mMainExpensesRecyclerView?.adapter = TransactionsListArrayAdapter(mObjects) {
             Log.e("CLICKED", "item ${it.transactionName}")
+        }
+
+        mUserProfilePictureView?.profileId = mUserId
+    }
+
+    private fun getUsersFacebookProfilePicture(userId: String?) : Bitmap {
+        val imageUrl : URL = URL("https://graph.facebook.com/$userId/picture?type=large")
+        val bitmap : Bitmap = BitmapFactory.decodeStream(imageUrl.openConnection().getInputStream())
+        return bitmap
+    }
+
+    //endregion
+
+    //region Inner Objects and Classes
+
+    companion object {
+        private val USER_ID = "user_id"
+
+        fun getActivityIntent(context: Context, userId: String?) : Intent {
+            var intent : Intent = Intent(context, MainActivity::class.java)
+            intent.putExtra(USER_ID, userId)
+            return intent
         }
     }
 
